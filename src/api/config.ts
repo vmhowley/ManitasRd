b096bbf899212baaaf40f7d43e7a0a2bf87621b8
vmhowley <vmhowleyh@gmail.com>
2025-07-08 20:07:26 -0400

    feat: Actualizar la URL base de la API para usar la variable de entorno

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
 // src/api/config.ts
-export const API_BASE_URL = 'http://localhost:5000'; // Usa la variable de entorno o localhost para desarrollo
+export const API_BASE_URL = import.meta.env.API_BASE_URL 
