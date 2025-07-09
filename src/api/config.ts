c0ba28f7865dde9df8c8eaa22dd28fad10ed3ac3
vmhowley <vmhowleyh@gmail.com>
2025-07-08 20:28:51 -0400

    feat: Actualizar la URL base de la API para usar la variable de entorno VITE_API_BASE_URL

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
 // src/api/config.ts
-export const API_BASE_URL = import.meta.env.BASE_URL
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Usa la variable de entorno o localhost para desarrollo
